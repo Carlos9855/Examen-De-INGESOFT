d511837f072851262910e8763f78bfaa404a3772
carlos camacho <josecarloscamacho55@gmail.com>
2021-05-25 10:23:24 -0400

    3

--- proyecto-base/template2_6_5-master/spec/fraccionar_spec.rb
+++ proyecto-base/template2_6_5-master/spec/fraccionar_spec.rb
@@ -6,9 +6,30 @@ RSpec.describe "fraccionar" do
         resultado = fraccionar_1(frac)
         expect(resultado).to eq('[5, 5]')  
     end  
-    it "deberia devolver '[5,5,2]' para el numero 10" do
+    it "deberia devolver '[5,5,2]' para el numero 12" do
         frac=12
         resultado = fraccionar_1(frac)
         expect(resultado).to eq('[5, 5, 2]')  
+    end
+    it "deberia devolver '[5,5,5,2,2]' para el numero 19" do
+        frac=19
+        resultado = fraccionar_1(frac)
+        expect(resultado).to eq('[5, 5, 5, 2, 2]')  
+    end
+    it "deberia devolver '[5, 5, 2, 1]' para el numero 13" do
+        frac=13
+        resultado = fraccionar_1(frac)
+        expect(resultado).to eq('[5, 5, 2, 1]')  
+    end
+
+    it "deberia devolver '[2,1]' para el numero 3" do
+        frac=3
+        resultado = fraccionar_1(frac)
+        expect(resultado).to eq('[2, 1]')  
+    end
+    it "deberia devolver '[1]' para el numero 1" do
+        frac=1
+        resultado = fraccionar_1(frac)
+        expect(resultado).to eq('[1]')  
     end
 end
